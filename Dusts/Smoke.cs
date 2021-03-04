5742aade5f7979ec446f3478d470afbbc9d8e10a
DarkJakDeveloper <mnqgermain@yahoo.ca>
2021-03-03 21:22:30 -0600

    Original code woohoo
    Got it back. Turns out it did push the first time

--- Dusts/Smoke.cs
+++ Dusts/Smoke.cs
@@ -1,31 +1,32 @@
-using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ModLoader;
 
 namespace RedMod.Dusts
 {
-	// Token: 0x02000007 RID: 7
-	public class Smoke : ModDust
-	{
-		// Token: 0x06000014 RID: 20 RVA: 0x0000295C File Offset: 0x00000B5C
-		public override void OnSpawn(Dust dust)
-		{
-			dust.noGravity = true;
-			dust.velocity *= 1f;
-			dust.velocity.Y = dust.velocity.Y - 0.5f;
-		}
+    /*
+        This is the actual smoke spawned by the projectile.
+    */
+    public class Smoke : ModDust
+    {
+        public override void OnSpawn(Dust dust)
+        {
+            dust.noGravity = true;
+            dust.velocity *= 1f;
+            dust.velocity.Y -= 0.5f;
+        }
 
-		// Token: 0x06000015 RID: 21 RVA: 0x00002990 File Offset: 0x00000B90
-		public override bool Update(Dust dust)
-		{
-			dust.position += dust.velocity;
-			dust.rotation += dust.velocity.X * 0.1f;
-			dust.scale -= 0.002f;
-			if (dust.scale < 0.02f)
-			{
-				dust.active = false;
-			}
-			return false;
-		}
-	}
+        public override bool Update(Dust dust)
+        {
+            dust.position += dust.velocity;
+            dust.rotation += dust.velocity.X * 0.1f;
+            dust.scale -= 0.002f;
+            if (dust.scale < 0.02f)
+            {
+                dust.active = false;
+            }
+            return false;
+        }
+    }
 }
--- Items/MantvySword.cs
+++ Items/MantvySword.cs
@@ -1,44 +1,39 @@
-using System;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace RedMod.Items
 {
-	// Token: 0x02000005 RID: 5
 	public class MantvySword : ModItem
 	{
-		// Token: 0x0600000C RID: 12 RVA: 0x00002736 File Offset: 0x00000936
-		public override void SetStaticDefaults()
+		public override void SetStaticDefaults() 
 		{
-			base.DisplayName.SetDefault("The Sword of Mantvydas");
-			base.Tooltip.SetDefault("Red's ultra sword.");
+			DisplayName.SetDefault("The Sword of Mantvydas"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("Red's ultra sword.");
 		}
 
-		// Token: 0x0600000D RID: 13 RVA: 0x00002758 File Offset: 0x00000958
-		public override void SetDefaults()
+		public override void SetDefaults() 
 		{
-			base.item.damage = 196;
-			base.item.melee = true;
-			base.item.width = 40;
-			base.item.height = 40;
-			base.item.useTime = 20;
-			base.item.useAnimation = 20;
-			base.item.useStyle = 1;
-			base.item.knockBack = 6f;
-			base.item.value = 10000;
-			base.item.rare = 2;
-			base.item.UseSound = SoundID.Item1;
-			base.item.autoReuse = true;
+			item.damage = 14 * 14; // hehe why not
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 6;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
 		}
 
-		// Token: 0x0600000E RID: 14 RVA: 0x00002809 File Offset: 0x00000A09
-		public override void AddRecipes()
+		public override void AddRecipes() 
 		{
-			ModRecipe modRecipe = new ModRecipe(base.mod);
-			modRecipe.AddIngredient(2, 10);
-			modRecipe.AddTile(18);
-			modRecipe.SetResult(this, 1);
-			modRecipe.AddRecipe();
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 10);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
 		}
 	}
 }
